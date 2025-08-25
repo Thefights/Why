5c2f59f66e6f1c7865b5c1474c37ad1182186219
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-08-25 21:04:15 +0700

    Config cloud image but i dont know can it run hehe

---
+++ BusinessLogicLayer/Implements/ImageService.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.S3;
+using BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace BusinessLogicLayer.Implements
+{
+    public class ImageService : IImageService
+    {
+        public async Task<string> UploadImageAsync(IFormFile file, string bucketName, IAmazonS3 s3Client)
+        {
+            var fileTransferUtility = new Amazon.S3.Transfer.TransferUtility(s3Client);
+            var fileExtension = Path.GetExtension(file.FileName);
+            var uniqueFileName = $"{Guid.NewGuid()}{fileExtension}";
+
+            using (var stream = file.OpenReadStream())
+            {
+                var uploadRequest = new Amazon.S3.Transfer.TransferUtilityUploadRequest
+                {
+                    InputStream = stream,
+                    Key = uniqueFileName,
+                    BucketName = bucketName,
+                    ContentType = file.ContentType
+                };
+                await fileTransferUtility.UploadAsync(uploadRequest);
+            }
+
+            string fileUrl = $"https://{bucketName}.s3.amazonaws.com/{uniqueFileName}";
+
+            return fileUrl;
+        }
+    }
+}
---
+++ BusinessLogicLayer/Interfaces/IImageService.cs
@@ -0,0 +1,10 @@
+﻿using Amazon.S3;
+using Microsoft.AspNetCore.Http;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<string> UploadImageAsync(IFormFile file, string bucketName, IAmazonS3 s3Client);
+    }
+}
