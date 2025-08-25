575f5c7490a75a3aff2ec3859da393864334751d
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-08-25 21:06:40 +0700

    bi ngu, nua tim cach tach ra sau

--- chaolong-sever/Program.cs
+++ chaolong-sever/Program.cs
@@ -1,4 +1,9 @@
+using Amazon.S3;
+using BusinessLogicLayer.Implements;
+using BusinessLogicLayer.Interfaces;
 using DataAccessLayer.Data;
+using DataAccessLayer.Repository.Base;
+using DataAccessLayer.Repository.IRepository.Base;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +15,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ChaoLongCoTham")));
 
 builder.Services.AddSwaggerGen();
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonS3>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IImageService, ImageService>();
+
 
 var app = builder.Build();
 
