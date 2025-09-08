f36e236f852acabf4358f9ae90f7a0e400f9d085
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-08 14:27:42 +0700

    Update regex for password and phone Change error message to match test case

--- BusinessLogicLayer/DTO/UserDTO/AuthUserRequestDTO.cs
+++ BusinessLogicLayer/DTO/UserDTO/AuthUserRequestDTO.cs
@@ -12,7 +12,7 @@ namespace BusinessLogicLayer.DTO.UserDTO
         public string Name { get; set; } = string.Empty;
 
         [Required]
-        [Phone(ErrorMessage = "Invalid {0}")]
+        [RegularExpression(@"^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4[0-9]|5[1|2|4-9]|6[0-3|9]|7[0-7]|8[0-9]|9[0-4|6|7|9])|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])([0-9]{7})$", ErrorMessage = "Invalid {0}, {0} have to be Vietnamese phone!")]
         public string Phone { get; set; } = string.Empty;
 
         [Required]
@@ -20,6 +20,7 @@ namespace BusinessLogicLayer.DTO.UserDTO
         public string Email { get; set; } = string.Empty;
 
         [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "{0} must be between 6 and 20 characters and contain one uppercase letter, one lowercase letter, one digit and one special character.")]
         public string Password { get; set; } = string.Empty;
 
         [JsonIgnore]
